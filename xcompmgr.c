1783d3bd50924ab4a74b86bc8b50c631d209071d
Dave Airlie <airlied@linux.ie>
2006-12-21 18:36:33 +1100

    increase version number to 1.1.3

--- xcompmgr.c
+++ xcompmgr.c
@@ -1838,7 +1838,7 @@ ev_window (XEvent *ev)
 void
 usage (char *program)
 {
-    fprintf (stderr, "%s v1.1.2\n", program);
+    fprintf (stderr, "%s v1.1.3\n", program);
     fprintf (stderr, "usage: %s [options]\n", program);
     fprintf (stderr, "Options\n");
     fprintf (stderr, "   -d display\n      Specifies which display should be managed.\n");
