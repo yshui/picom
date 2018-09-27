yshui/picom

commit fcef5e706d52fe1368c1487a76ef47ab90804020

Some cleanup work
* Remove NO_C2 option
* Split configuration related functions into their own file
* Drop support for libconfig < 1.4
* Fix dependencies in Makefile

src/config.h
@@ -1,0 +1,32 @@
+#pragma once
+
+#include <stdbool.h>
+
+#include <libconfig.h>
+
+#include "common.h"
+
+bool parse_long(const char *, long *);
+const char *parse_matrix_readnum(const char *, double *);
+XFixed *parse_matrix(session_t *, const char *, const char **);
+XFixed *parse_conv_kern(session_t *, const char *, const char **);
+bool parse_conv_kern_lst(session_t *, const char *, XFixed **, int);
+bool parse_geometry(session_t *, const char *, geometry_t *);
+bool parse_rule_opacity(session_t *, const char *);
+
+/**
+ * Add a pattern to a condition linked list.
+ */
+bool condlst_add(session_t *, c2_lptr_t **, const char *);
+
+#ifdef CONFIG_LIBCONFIG
+FILE *
+open_config_file(char *cpath, char **path);
+
+void
+parse_cfg_condlst(session_t *ps, const config_t *pcfg, c2_lptr_t **pcondlst,
+    const char *name);
+
+void
+parse_config(session_t *ps, struct options_tmp *pcfgtmp);
+#endif

.gitignore
@@ -2,8 +2,6 @@
 .deps
 aclocal.m4
 autom4te.cache
-config.h
-config.h.in
 config.log
 config.status
 configure

Makefile
@@ -13,10 +13,10 @@ PACKAGES = x11 xcomposite xfixes xdamage xrender xext xrandr
 LIBS = -lm -lrt
 INCS =
 
-OBJS = compton.o
+OBJS = compton.o config.o
 
 # === Configuration flags ===
-CFG = -std=c99
+CFG = -std=c99 -D_GNU_SOURCE
 
 # ==== Xinerama ====
 # Enables support for --xinerama-shadow-crop
@@ -30,10 +30,14 @@ endif
 ifeq "$(NO_LIBCONFIG)" ""
   CFG += -DCONFIG_LIBCONFIG
   PACKAGES += libconfig
+  OBJS += config_libconfig.o
 
   # libconfig-1.3* does not define LIBCONFIG_VER* macros, so we use
   # pkg-config to determine its version here
-  CFG += $(shell pkg-config --atleast-version=1.4 libconfig || echo '-DCONFIG_LIBCONFIG_LEGACY')
+  LIBCONFIG_VER = $(shell pkg-config --atleast-version=1.4 libconfig; echo $$?)
+  ifeq ($LIBCONFIG_VER, 1)
+    $(error "Your libconfig is too old, at least 1.4 is required")
+  endif
 endif
 
 # ==== PCRE regular expression ====
@@ -90,12 +94,7 @@ ifeq "$(NO_XSYNC)" ""
   CFG += -DCONFIG_XSYNC
 endif
 
-# ==== C2 ====
-# Enable window condition support
-ifeq "$(NO_C2)" ""
-  CFG += -DCONFIG_C2
-  OBJS += c2.o
-endif
+OBJS += c2.o
 
 # ==== X resource checker ====
 # Enable X resource leakage checking (Pixmap only, presently)
@@ -141,7 +140,15 @@ MANPAGES_HTML = $(addsuffix .html,$(MANPAGES))
 src/.clang_complete: Makefile
 	@(for i in $(filter-out -O% -DNDEBUG, $(CFG) $(CPPFLAGS) $(CFLAGS) $(INCS)); do echo "$$i"; done) > $@
 
-%.o: src/%.c src/%.h src/common.h
+.deps:
+	mkdir -p .deps
+
+%.o: src/%.c .deps
+	$(eval DEP=$(addprefix .deps/,$(@:.o=.d)))
+	@set -e; rm -f $(DEP); \
+	  $(CC) -M $(CPPFLAGS) $< > $@.$$$$; \
+	  sed 's,\($*\)\.o[ :]*,\1.o $@ : ,g' < $@.$$$$ > $(DEP); \
+	  rm -f $@.$$$$
 	$(CC) $(CFG) $(CPPFLAGS) $(CFLAGS) $(INCS) -c src/$*.c
 
 compton: $(OBJS)
@@ -183,8 +190,10 @@ endif
 
 clean:
 	@rm -f $(OBJS) compton $(MANPAGES) $(MANPAGES_HTML) .clang_complete
+	@rm -rf .deps
 
 version:
 	@echo "$(COMPTON_VERSION)"
 
 .PHONY: uninstall clean docs version
+include $(addprefix .deps/,$(sources:.c=.d))

src/common.h
@@ -8,8 +8,7 @@
  *
  */
 
-#ifndef COMPTON_COMMON_H
-#define COMPTON_COMMON_H
+#pragma once
 
 // === Options ===
 
@@ -42,8 +41,6 @@
 // #define CONFIG_REGEX_PCRE_JIT 1
 // Whether to enable parsing of configuration files using libconfig.
 // #define CONFIG_LIBCONFIG 1
-// Whether we are using a legacy version of libconfig (1.3.x).
-// #define CONFIG_LIBCONFIG_LEGACY 1
 // Whether to enable DRM VSync support
 // #define CONFIG_VSYNC_DRM 1
 // Whether to enable OpenGL support
@@ -54,16 +51,10 @@
 // #define CONFIG_VSYNC_OPENGL_FBO 1
 // Whether to enable DBus support with libdbus.
 // #define CONFIG_DBUS 1
-// Whether to enable condition support.
-// #define CONFIG_C2 1
 // Whether to enable X Sync support.
 // #define CONFIG_XSYNC 1
 // Whether to enable GLX Sync support.
 // #define CONFIG_GLX_XSYNC 1
-
-#if !defined(CONFIG_C2) && defined(DEBUG_C2)
-#error Cannot enable c2 debugging without c2 support.
-#endif
 
 #if (!defined(CONFIG_XSYNC) || !defined(CONFIG_VSYNC_OPENGL)) && defined(CONFIG_GLX_SYNC)
 #error Cannot enable GL sync without X Sync / OpenGL support.
@@ -80,8 +71,6 @@
 // === Includes ===
 
 // For some special functions
-#define _GNU_SOURCE
-
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -114,12 +103,6 @@
 // thanks to consolers for reporting
 #ifndef PictOpDifference
 #define PictOpDifference 0x39
-#endif
-
-// libconfig
-#ifdef CONFIG_LIBCONFIG
-#include <libgen.h>
-#include <libconfig.h>
 #endif
 
 // libdbus
@@ -2483,7 +2466,6 @@ opts_set_no_fading_openclose(session_t *ps, bool newval);
 //!@}
 #endif
 
-#ifdef CONFIG_C2
 /** @name c2
  */
 ///@{
@@ -2503,7 +2485,6 @@ c2_matchd(session_t *ps, win *w, const c2_lptr_t *condlst,
 
 #define c2_match(ps, w, condlst, cache) c2_matchd((ps), (w), (condlst), \
     (cache), NULL)
-#endif
 
 ///@}
 
@@ -2564,4 +2545,14 @@ hexdump(const char *data, int len) {
   fflush(stdout);
 }
 
-#endif
+/**
+ * Set a <code>bool</code> array of all wintypes to true.
+ */
+static inline void
+wintype_arr_enable(bool arr[]) {
+  wintype_t i;
+
+  for (i = 0; i < NUM_WINTYPES; ++i) {
+    arr[i] = true;
+  }
+}

src/compton.h
@@ -70,18 +70,6 @@ sub_unslong(unsigned long a, unsigned long b) {
 }
 
 /**
- * Set a <code>bool</code> array of all wintypes to true.
- */
-static inline void
-wintype_arr_enable(bool arr[]) {
-  wintype_t i;
-
-  for (i = 0; i < NUM_WINTYPES; ++i) {
-    arr[i] = true;
-  }
-}
-
-/**
  * Set a <code>switch_t</code> array of all unset wintypes to true.
  */
 static inline void
@@ -186,10 +174,8 @@ free_damage(session_t *ps, Damage *p) {
  */
 static inline void
 free_wincondlst(c2_lptr_t **pcondlst) {
-#ifdef CONFIG_C2
   while ((*pcondlst = c2_free_lptr(*pcondlst)))
     continue;
-#endif
 }
 
 /**
@@ -585,15 +571,8 @@ win_validate_pixmap(session_t *ps, win *w) {
  */
 static inline bool
 win_match(session_t *ps, win *w, c2_lptr_t *condlst, const c2_lptr_t **cache) {
-#ifdef CONFIG_C2
   return c2_match(ps, w, condlst, cache);
-#else
-  return false;
-#endif
 }
-
-static bool
-condlst_add(session_t *ps, c2_lptr_t **pcondlst, const char *pattern);
 
 static long
 determine_evmask(session_t *ps, Window wid, win_evmode_t mode);
@@ -1173,54 +1152,6 @@ ev_handle(session_t *ps, XEvent *ev);
 
 static bool
 fork_after(session_t *ps);
-
-#ifdef CONFIG_LIBCONFIG
-/**
- * Wrapper of libconfig's <code>config_lookup_int</code>.
- *
- * To convert <code>int</code> value <code>config_lookup_bool</code>
- * returns to <code>bool</code>.
- */
-static inline void
-lcfg_lookup_bool(const config_t *config, const char *path,
-    bool *value) {
-  int ival;
-
-  if (config_lookup_bool(config, path, &ival))
-    *value = ival;
-}
-
-/**
- * Wrapper of libconfig's <code>config_lookup_int</code>.
- *
- * To deal with the different value types <code>config_lookup_int</code>
- * returns in libconfig-1.3 and libconfig-1.4.
- */
-static inline int
-lcfg_lookup_int(const config_t *config, const char *path, int *value) {
-#ifndef CONFIG_LIBCONFIG_LEGACY
-  return config_lookup_int(config, path, value);
-#else
-  long lval;
-  int ret;
-
-  if ((ret = config_lookup_int(config, path, &lval)))
-    *value = lval;
-
-  return ret;
-#endif
-}
-
-static FILE *
-open_config_file(char *cpath, char **path);
-
-static void
-parse_cfg_condlst(session_t *ps, const config_t *pcfg, c2_lptr_t **pcondlst,
-    const char *name);
-
-static void
-parse_config(session_t *ps, struct options_tmp *pcfgtmp);
-#endif
 
 static void
 get_cfg(session_t *ps, int argc, char *const *argv, bool first_pass);

src/config.c
@@ -1,0 +1,320 @@
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "common.h"
+#include "config.h"
+
+/**
+ * Parse a long number.
+ */
+bool
+parse_long(const char *s, long *dest) {
+  const char *endptr = NULL;
+  long val = strtol(s, (char **) &endptr, 0);
+  if (!endptr || endptr == s) {
+    printf_errf("(\"%s\"): Invalid number.", s);
+    return false;
+  }
+  while (isspace(*endptr))
+    ++endptr;
+  if (*endptr) {
+    printf_errf("(\"%s\"): Trailing characters.", s);
+    return false;
+  }
+  *dest = val;
+  return true;
+}
+
+/**
+ * Parse a floating-point number in matrix.
+ */
+const char *
+parse_matrix_readnum(const char *src, double *dest) {
+  char *pc = NULL;
+  double val = strtod(src, &pc);
+  if (!pc || pc == src) {
+    printf_errf("(\"%s\"): No number found.", src);
+    return src;
+  }
+
+  while (*pc && (isspace(*pc) || ',' == *pc))
+    ++pc;
+
+  *dest = val;
+
+  return pc;
+}
+
+/**
+ * Parse a matrix.
+ */
+XFixed *
+parse_matrix(session_t *ps, const char *src, const char **endptr) {
+  int wid = 0, hei = 0;
+  const char *pc = NULL;
+  XFixed *matrix = NULL;
+
+  // Get matrix width and height
+  {
+    double val = 0.0;
+    if (src == (pc = parse_matrix_readnum(src, &val)))
+      goto parse_matrix_err;
+    src = pc;
+    wid = val;
+    if (src == (pc = parse_matrix_readnum(src, &val)))
+      goto parse_matrix_err;
+    src = pc;
+    hei = val;
+  }
+
+  // Validate matrix width and height
+  if (wid <= 0 || hei <= 0) {
+    printf_errf("(): Invalid matrix width/height.");
+    goto parse_matrix_err;
+  }
+  if (!(wid % 2 && hei % 2)) {
+    printf_errf("(): Width/height not odd.");
+    goto parse_matrix_err;
+  }
+  if (wid > 16 || hei > 16)
+    printf_errf("(): Matrix width/height too large, may slow down"
+                "rendering, and/or consume lots of memory");
+
+  // Allocate memory
+  matrix = calloc(wid * hei + 2, sizeof(XFixed));
+  if (!matrix) {
+    printf_errf("(): Failed to allocate memory for matrix.");
+    goto parse_matrix_err;
+  }
+
+  // Read elements
+  {
+    int skip = hei / 2 * wid + wid / 2;
+    bool hasneg = false;
+    for (int i = 0; i < wid * hei; ++i) {
+      // Ignore the center element
+      if (i == skip) {
+        matrix[2 + i] = XDoubleToFixed(0);
+        continue;
+      }
+      double val = 0;
+      if (src == (pc = parse_matrix_readnum(src, &val)))
+        goto parse_matrix_err;
+      src = pc;
+      if (val < 0) hasneg = true;
+      matrix[2 + i] = XDoubleToFixed(val);
+    }
+    if (BKEND_XRENDER == ps->o.backend && hasneg)
+      printf_errf("(): A convolution kernel with negative values "
+          "may not work properly under X Render backend.");
+  }
+
+  // Detect trailing characters
+  for ( ;*pc && ';' != *pc; ++pc)
+    if (!isspace(*pc) && ',' != *pc) {
+      printf_errf("(): Trailing characters in matrix string.");
+      goto parse_matrix_err;
+    }
+
+  // Jump over spaces after ';'
+  if (';' == *pc) {
+    ++pc;
+    while (*pc && isspace(*pc))
+      ++pc;
+  }
+
+  // Require an end of string if endptr is not provided, otherwise
+  // copy end pointer to endptr
+  if (endptr)
+    *endptr = pc;
+  else if (*pc) {
+    printf_errf("(): Only one matrix expected.");
+    goto parse_matrix_err;
+  }
+
+  // Fill in width and height
+  matrix[0] = XDoubleToFixed(wid);
+  matrix[1] = XDoubleToFixed(hei);
+
+  return matrix;
+
+parse_matrix_err:
+  free(matrix);
+  return NULL;
+}
+
+/**
+ * Parse a convolution kernel.
+ */
+XFixed *
+parse_conv_kern(session_t *ps, const char *src, const char **endptr) {
+  return parse_matrix(ps, src, endptr);
+}
+
+/**
+ * Parse a list of convolution kernels.
+ */
+bool
+parse_conv_kern_lst(session_t *ps, const char *src, XFixed **dest, int max) {
+  static const struct {
+    const char *name;
+    const char *kern_str;
+  } CONV_KERN_PREDEF[] = {
+    { "3x3box", "3,3,1,1,1,1,1,1,1,1," },
+    { "5x5box", "5,5,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1," },
+    { "7x7box", "7,7,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1," },
+    { "3x3gaussian", "3,3,0.243117,0.493069,0.243117,0.493069,0.493069,0.243117,0.493069,0.243117," },
+    { "5x5gaussian", "5,5,0.003493,0.029143,0.059106,0.029143,0.003493,0.029143,0.243117,0.493069,0.243117,0.029143,0.059106,0.493069,0.493069,0.059106,0.029143,0.243117,0.493069,0.243117,0.029143,0.003493,0.029143,0.059106,0.029143,0.003493," },
+    { "7x7gaussian", "7,7,0.000003,0.000102,0.000849,0.001723,0.000849,0.000102,0.000003,0.000102,0.003493,0.029143,0.059106,0.029143,0.003493,0.000102,0.000849,0.029143,0.243117,0.493069,0.243117,0.029143,0.000849,0.001723,0.059106,0.493069,0.493069,0.059106,0.001723,0.000849,0.029143,0.243117,0.493069,0.243117,0.029143,0.000849,0.000102,0.003493,0.029143,0.059106,0.029143,0.003493,0.000102,0.000003,0.000102,0.000849,0.001723,0.000849,0.000102,0.000003," },
+    { "9x9gaussian", "9,9,0.000000,0.000000,0.000001,0.000006,0.000012,0.000006,0.000001,0.000000,0.000000,0.000000,0.000003,0.000102,0.000849,0.001723,0.000849,0.000102,0.000003,0.000000,0.000001,0.000102,0.003493,0.029143,0.059106,0.029143,0.003493,0.000102,0.000001,0.000006,0.000849,0.029143,0.243117,0.493069,0.243117,0.029143,0.000849,0.000006,0.000012,0.001723,0.059106,0.493069,0.493069,0.059106,0.001723,0.000012,0.000006,0.000849,0.029143,0.243117,0.493069,0.243117,0.029143,0.000849,0.000006,0.000001,0.000102,0.003493,0.029143,0.059106,0.029143,0.003493,0.000102,0.000001,0.000000,0.000003,0.000102,0.000849,0.001723,0.000849,0.000102,0.000003,0.000000,0.000000,0.000000,0.000001,0.000006,0.000012,0.000006,0.000001,0.000000,0.000000," },
+    { "11x11gaussian", "11,11,0.000000,0.000000,0.000000,0.000000,0.000000,0.000000,0.000000,0.000000,0.000000,0.000000,0.000000,0.000000,0.000000,0.000000,0.000001,0.000006,0.000012,0.000006,0.000001,0.000000,0.000000,0.000000,0.000000,0.000000,0.000003,0.000102,0.000849,0.001723,0.000849,0.000102,0.000003,0.000000,0.000000,0.000000,0.000001,0.000102,0.003493,0.029143,0.059106,0.029143,0.003493,0.000102,0.000001,0.000000,0.000000,0.000006,0.000849,0.029143,0.243117,0.493069,0.243117,0.029143,0.000849,0.000006,0.000000,0.000000,0.000012,0.001723,0.059106,0.493069,0.493069,0.059106,0.001723,0.000012,0.000000,0.000000,0.000006,0.000849,0.029143,0.243117,0.493069,0.243117,0.029143,0.000849,0.000006,0.000000,0.000000,0.000001,0.000102,0.003493,0.029143,0.059106,0.029143,0.003493,0.000102,0.000001,0.000000,0.000000,0.000000,0.000003,0.000102,0.000849,0.001723,0.000849,0.000102,0.000003,0.000000,0.000000,0.000000,0.000000,0.000000,0.000001,0.000006,0.000012,0.000006,0.000001,0.000000,0.000000,0.000000,0.000000,0.000000,0.000000,0.000000,0.000000,0.000000,0.000000,0.000000,0.000000,0.000000,0.000000," },
+  };
+  for (int i = 0;
+      i < sizeof(CONV_KERN_PREDEF) / sizeof(CONV_KERN_PREDEF[0]); ++i)
+    if (!strcmp(CONV_KERN_PREDEF[i].name, src))
+      return parse_conv_kern_lst(ps, CONV_KERN_PREDEF[i].kern_str, dest, max);
+
+  int i = 0;
+  const char *pc = src;
+
+  // Free old kernels
+  for (i = 0; i < max; ++i) {
+    free(dest[i]);
+    dest[i] = NULL;
+  }
+
+  // Continue parsing until the end of source string
+  i = 0;
+  while (pc && *pc && i < max - 1) {
+    if (!(dest[i++] = parse_conv_kern(ps, pc, &pc)))
+      return false;
+  }
+
+  if (*pc) {
+    printf_errf("(): Too many blur kernels!");
+    return false;
+  }
+
+  return true;
+}
+
+/**
+ * Parse a X geometry.
+ */
+bool
+parse_geometry(session_t *ps, const char *src, geometry_t *dest) {
+  geometry_t geom = { .wid = -1, .hei = -1, .x = -1, .y = -1 };
+  long val = 0L;
+  char *endptr = NULL;
+
+#define T_STRIPSPACE() do { \
+  while (*src && isspace(*src)) ++src; \
+  if (!*src) goto parse_geometry_end; \
+} while(0)
+
+  T_STRIPSPACE();
+
+  // Parse width
+  // Must be base 10, because "0x0..." may appear
+  if (!('+' == *src || '-' == *src)) {
+    val = strtol(src, &endptr, 10);
+    if (endptr && src != endptr) {
+      geom.wid = val;
+      assert(geom.wid >= 0);
+      src = endptr;
+    }
+    T_STRIPSPACE();
+  }
+
+  // Parse height
+  if ('x' == *src) {
+    ++src;
+    val = strtol(src, &endptr, 10);
+    if (endptr && src != endptr) {
+      geom.hei = val;
+      if (geom.hei < 0) {
+        printf_errf("(\"%s\"): Invalid height.", src);
+        return false;
+      }
+      src = endptr;
+    }
+    T_STRIPSPACE();
+  }
+
+  // Parse x
+  if ('+' == *src || '-' == *src) {
+    val = strtol(src, &endptr, 10);
+    if (endptr && src != endptr) {
+      geom.x = val;
+      if ('-' == *src && geom.x <= 0)
+        geom.x -= 2;
+      src = endptr;
+    }
+    T_STRIPSPACE();
+  }
+
+  // Parse y
+  if ('+' == *src || '-' == *src) {
+    val = strtol(src, &endptr, 10);
+    if (endptr && src != endptr) {
+      geom.y = val;
+      if ('-' == *src && geom.y <= 0)
+        geom.y -= 2;
+      src = endptr;
+    }
+    T_STRIPSPACE();
+  }
+
+  if (*src) {
+    printf_errf("(\"%s\"): Trailing characters.", src);
+    return false;
+  }
+
+parse_geometry_end:
+  *dest = geom;
+  return true;
+}
+
+/**
+ * Parse a list of opacity rules.
+ */
+bool parse_rule_opacity(session_t *ps, const char *src) {
+  // Find opacity value
+  char *endptr = NULL;
+  long val = strtol(src, &endptr, 0);
+  if (!endptr || endptr == src) {
+    printf_errf("(\"%s\"): No opacity specified?", src);
+    return false;
+  }
+  if (val > 100 || val < 0) {
+    printf_errf("(\"%s\"): Opacity %ld invalid.", src, val);
+    return false;
+  }
+
+  // Skip over spaces
+  while (*endptr && isspace(*endptr))
+    ++endptr;
+  if (':' != *endptr) {
+    printf_errf("(\"%s\"): Opacity terminator not found.", src);
+    return false;
+  }
+  ++endptr;
+
+  // Parse pattern
+  // I hope 1-100 is acceptable for (void *)
+  return c2_parsed(ps, &ps->o.opacity_rules, endptr, (void *) val);
+}
+
+/**
+ * Add a pattern to a condition linked list.
+ */
+bool
+condlst_add(session_t *ps, c2_lptr_t **pcondlst, const char *pattern) {
+  if (!pattern)
+    return false;
+
+  if (!c2_parse(ps, pcondlst, pattern))
+    exit(1);
+
+  return true;
+}

commit 0a98ef79ef20eb2bd28df0cf73fb93d503d68ab1

Fix build with NO_LIBCONFIG=1
This got broken in commit fcef5e706d52fe.

Signed-off-by: Uli Schlachter <psychon@znc.in>

src/config.h
@@ -2,7 +2,9 @@
 
 #include <stdbool.h>
 
+#ifdef CONFIG_LIBCONFIG
 #include <libconfig.h>
+#endif
 
 #include "common.h"
 