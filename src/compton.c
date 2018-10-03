5eb474ef0e7d3510960f046e446898d13a7dfec2
Uli Schlachter <psychon@znc.in>
2018-10-03 14:05:33 +0200

    Replace XDestroyWindow with xcb_destroy_window
    Signed-off-by: Uli Schlachter <psychon@znc.in>

--- src/compton.c
+++ src/compton.c
@@ -5601,7 +5601,7 @@ session_destroy(session_t *ps) {
 
   // Free reg_win
   if (ps->reg_win) {
-    XDestroyWindow(ps->dpy, ps->reg_win);
+    xcb_destroy_window(c, ps->reg_win);
     ps->reg_win = None;
   }
 
