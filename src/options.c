4f1dddd072252707037324d6d000a813b4f454be
Yuxuan Shui <yshuiv7@gmail.com>
2018-12-24 19:58:25 +0000

    -C and -G *disables* shadow, not enabling them
    Well, I made a mistake.
    
    Fixes #79
    
    Signed-off-by: Yuxuan Shui <yshuiv7@gmail.com>

--- src/options.c
+++ src/options.c
@@ -551,11 +551,11 @@ void get_cfg(options_t *opt, int argc, char *const *argv, bool shadow_enable,
 		case 'c': shadow_enable = true; break;
 		case 'C':
 			winopt_mask[WINTYPE_DOCK].shadow = true;
-			opt->wintype_option[WINTYPE_DOCK].shadow = true;
+			opt->wintype_option[WINTYPE_DOCK].shadow = false;
 			break;
 		case 'G':
 			winopt_mask[WINTYPE_DND].shadow = true;
-			opt->wintype_option[WINTYPE_DND].shadow = true;
+			opt->wintype_option[WINTYPE_DND].shadow = false;
 			break;
 		case 'm':;
 			double tmp;
