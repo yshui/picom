e986ebceda4ab8035e0ca298d806211262b3089f
Yuxuan Shui <yshuiv7@gmail.com>
2019-02-23 00:25:10 +0000

    Allow win_on_win_size_change to be called on UNMAPPED windows
    This can happen when the window has just been created
    
    Signed-off-by: Yuxuan Shui <yshuiv7@gmail.com>

--- src/win.c
+++ src/win.c
@@ -623,7 +623,7 @@ void win_on_win_size_change(session_t *ps, win *w) {
       backend_list[ps->o.backend]->release_win(ps->backend_data, ps, w, w->win_data);
       w->win_data = backend_list[ps->o.backend]->prepare_win(ps->backend_data, ps, w);
     } else {
-      assert(false);
+      assert(w->state == WSTATE_UNMAPPED);
     }
   } else {
     free_paint(ps, &w->shadow_paint);
