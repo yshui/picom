93e4a53d48bf5127f860ece2c6922063f431938e
Yuxuan Shui <yshuiv7@gmail.com>
2019-05-05 04:55:42 +0100

    cache: fix memory leak, add missing function
    Signed-off-by: Yuxuan Shui <yshuiv7@gmail.com>

--- src/cache.c
+++ src/cache.c
@@ -34,6 +34,7 @@ static inline void _cache_invalidate(struct cache *c, struct cache_entry *e) {
 	if (c->free) {
 		c->free(c->user_data, e->value);
 	}
+	free(e->key);
 	HASH_DEL(c->entries, e);
 	free(e);
 }
@@ -59,4 +60,12 @@ void *cache_free(struct cache *c) {
 	cache_invalidate_all(c);
 	free(c);
 	return ret;
+}
+
+struct cache *new_cache(void *ud, cache_getter_t getter, cache_free_t f) {
+	auto c = ccalloc(1, struct cache);
+	c->user_data = ud;
+	c->getter = getter;
+	c->free = f;
+	return c;
 }
