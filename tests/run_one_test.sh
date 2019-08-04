c8d6a5cae50391e457de41305f74fe1e9775c7c2
Yuxuan Shui <yshuiv7@gmail.com>
2019-08-05 00:05:23 +0100

    tests: print compton log after tests finish
    Signed-off-by: Yuxuan Shui <yshuiv7@gmail.com>

--- tests/run_one_test.sh
+++ tests/run_one_test.sh
@@ -6,10 +6,12 @@ fi
 echo "Running test $2"
 
 # TODO keep the log file, and parse it to see if test is successful
-($1 --experimental-backends --backend dummy --log-level=debug --log-file=/dev/null) &
+($1 --experimental-backends --backend dummy --log-level=debug --log-file=$PWD/log) &
 compton_pid=$!
 $2
 
 kill -INT $compton_pid
+cat log
+rm log
 
 
