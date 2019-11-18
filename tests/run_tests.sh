db2c9ff0be619d958165a4126ba3d68630d9c261
Yuxuan Shui <yshuiv7@gmail.com>
2019-11-18 20:53:49 +0000

    tests: add testcase for another case of #239
    The compositor crashes when a window does:
    has shadow -> no shadow -> has shadow
    
    This is because when the shadow is turned off, the shadow image is not
    freed, and this is catched by a later assertion.
    
    Signed-off-by: Yuxuan Shui <yshuiv7@gmail.com>

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -6,3 +6,4 @@ cd $(dirname $0)
 ./run_one_test.sh $exe configs/empty.conf testcases/basic.py
 ./run_one_test.sh $exe configs/issue239.conf testcases/issue239.py
 ./run_one_test.sh $exe configs/issue239_2.conf testcases/issue239_2.py
+./run_one_test.sh $exe configs/issue239_3.conf testcases/issue239_3.py
