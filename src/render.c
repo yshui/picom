45f530f26fbc2a7b33f9cd92bfd11314a8c86a57
Samuel Hand <samuel.d.hand@gmail.com>
2020-04-04 03:53:22 +0100

    Fix (another) memory leak

--- src/render.c
+++ src/render.c
@@ -805,10 +805,11 @@ win_paint_shadow(session_t *ps, struct managed_win *w, region_t *reg_paint) {
 		                              .height = to_u16_checked(w->heightb)};
 		xcb_render_fill_rectangles(ps->c, XCB_RENDER_PICT_OP_SRC, td, trans, 1, &rect);
 
-		xcb_render_trapezoids(
-		    ps->c, XCB_RENDER_PICT_OP_OVER,
-		    solid_picture(ps->c, ps->root, false, 1, 0, 0, 0), td,
-		    x_get_pictfmt_for_standard(ps->c, XCB_PICT_STANDARD_A_8), 0, 0, n, traps);
+		auto solid = solid_picture(ps->c, ps->root, false, 1, 0, 0, 0);
+		xcb_render_trapezoids(ps->c, XCB_RENDER_PICT_OP_OVER, solid, td,
+		                      x_get_pictfmt_for_standard(ps->c, XCB_PICT_STANDARD_A_8),
+		                      0, 0, n, traps);
+		xcb_render_free_picture(ps->c, solid);
 	}
 
 	clip_t clip = {.pict = td, -(w->shadow_dx), .y = -(w->shadow_dy)};
@@ -955,11 +956,13 @@ win_blur_background(session_t *ps, struct managed_win *w, xcb_render_picture_t t
 			xcb_render_fill_rectangles(ps->c, XCB_RENDER_PICT_OP_SRC, td,
 			                           trans, 1, &rect);
 
+			auto solid = solid_picture(ps->c, ps->root, false, 1, 0, 0, 0);
+
 			xcb_render_trapezoids(
-			    ps->c, XCB_RENDER_PICT_OP_OVER,
-			    solid_picture(ps->c, ps->root, false, 1, 0, 0, 0), td,
+			    ps->c, XCB_RENDER_PICT_OP_OVER, solid, td,
 			    x_get_pictfmt_for_standard(ps->c, XCB_PICT_STANDARD_A_8), 0,
 			    0, n, traps);
+			xcb_render_free_picture(ps->c, solid);
 		}
 
 		// Minimize the region we try to blur, if the window itself is not
@@ -978,6 +981,9 @@ win_blur_background(session_t *ps, struct managed_win *w, xcb_render_picture_t t
 		pixman_region32_translate(&reg_blur, -x, -y);
 		xr_blur_dst(ps, tgt_buffer, x, y, wid, hei, ps->blur_kerns_cache,
 		            ps->o.blur_kernel_count, &reg_blur, td);
+		if (td) {
+			xcb_render_free_picture(ps->c, td);
+		}
 		pixman_region32_clear(&reg_blur);
 	} break;
 #ifdef CONFIG_OPENGL
