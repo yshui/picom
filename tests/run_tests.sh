397710ac27907f8e799137774c0fc9a54662bb83
Yuxuan Shui <yshuiv7@gmail.com>
2020-04-05 02:52:12 +0100

    tests: add testcase for #314
    This should be fixed by merging #364.
    
    Thanks @tryone144 for pointing out the root cause.
    
    Signed-off-by: Yuxuan Shui <yshuiv7@gmail.com>

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -9,3 +9,4 @@ cd $(dirname $0)
 ./run_one_test.sh $exe configs/issue239_2.conf testcases/issue239_2.py
 ./run_one_test.sh $exe configs/issue239_3.conf testcases/issue239_3.py
 ./run_one_test.sh $exe configs/issue239_3.conf testcases/issue239_3_norefresh.py
+./run_one_test.sh $exe configs/issue314.conf testcases/issue314.py
