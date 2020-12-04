c50e44a8f47f5a8c8d7597d9bfe9db1ab238f654
Yuxuan Shui <yshuiv7@gmail.com>
2020-12-04 01:52:19 +0000

    win: downgrade a debug log to trace log
    Signed-off-by: Yuxuan Shui <yshuiv7@gmail.com>

--- src/win.c
+++ src/win.c
@@ -434,7 +434,7 @@ void win_process_update_flags(session_t *ps, struct managed_win *w) {
 	// Whether the window was visible before we process the mapped flag. i.e. is the
 	// window just mapped.
 	bool was_visible = win_is_real_visible(w);
-	log_debug("Processing flags for window %#010x (%s), was visible: %d", w->base.id,
+	log_trace("Processing flags for window %#010x (%s), was visible: %d", w->base.id,
 	          w->name, was_visible);
 
 	if (win_check_flags_all(w, WIN_FLAGS_MAPPED)) {
