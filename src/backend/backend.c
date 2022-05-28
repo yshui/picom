1972d9585f3cb3336441e7f4a9cb5c6d5f684cc9
Daniel Csillag <dccsillag@gmail.com>
2022-05-28 00:48:21 -0300

    Fix window size animations with rounded borders
    This is a slightly hacky solution in that it does not properly animate
    the borders changing; however, what actually happens in the borders is
    barely noticeable (especially with non snail-paced animation speeds), so
    it seems to be fine.

--- src/backend/backend.c
+++ src/backend/backend.c
@@ -394,10 +394,6 @@ void paint_all_new(session_t *ps, struct managed_win *t, bool ignore_damage) {
 			ps->backend_data->ops->set_image_property(
 			    ps->backend_data, IMAGE_PROPERTY_CORNER_RADIUS, w->win_image,
 			    (double[]){w->corner_radius});
-			if (w->old_win_image)
-				ps->backend_data->ops->set_image_property(
-				    ps->backend_data, IMAGE_PROPERTY_CORNER_RADIUS,
-				    w->old_win_image, (double[]){w->corner_radius});
 			if (w->corner_radius) {
 				int border_width = w->g.border_width;
 				if (border_width == 0) {
@@ -409,10 +405,17 @@ void paint_all_new(session_t *ps, struct managed_win *t, bool ignore_damage) {
 				ps->backend_data->ops->set_image_property(
 				    ps->backend_data, IMAGE_PROPERTY_BORDER_WIDTH,
 				    w->win_image, &border_width);
-				if (w->old_win_image)
+				if (w->old_win_image) {
+					// TODO(dccsillag): explain why the following is
+					// "necessary"
+					double zero = 0.0;
 					ps->backend_data->ops->set_image_property(
 					    ps->backend_data, IMAGE_PROPERTY_BORDER_WIDTH,
-					    w->old_win_image, &border_width);
+					    w->old_win_image, &zero);
+					ps->backend_data->ops->set_image_property(
+					    ps->backend_data, IMAGE_PROPERTY_CORNER_RADIUS,
+					    w->old_win_image, &zero);
+				}
 			}
 		}
 
