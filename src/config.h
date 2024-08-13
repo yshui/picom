yshui/picom

commit cdf25b07dc8dd41985c8987df793f4d17ebb5261

wm/win: separate per-window options into their own struct
It's easier to just store them together, since the same group of
information is needed for parsed window rules, and for window rendering.

Signed-off-by: Yuxuan Shui <yshuiv7@gmail.com>

src/config.h
@@ -10,6 +10,7 @@
 
 #include <ctype.h>
 #include <stdbool.h>
+#include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
 #include <xcb/render.h>        // for xcb_render_fixed_t, XXX
@@ -135,6 +136,67 @@ struct included_config_file {
 	char *path;
 	struct list_node siblings;
 };
+
+struct window_maybe_options {
+	/// Radius of rounded window corners, -1 means not set.
+	int corner_radius;
+
+	/// Window opacity, NaN means not set.
+	double opacity;
+
+	/// Name of the custom fragment shader for this window. NULL means not set.
+	const struct shader_info *shader;
+
+	/// Whether transparent clipping is excluded by the rules.
+	enum tristate transparent_clipping;
+	/// Whether a window has shadow.
+	enum tristate shadow;
+	/// Whether to invert window color.
+	enum tristate invert_color;
+	/// Whether to blur window background.
+	enum tristate blur_background;
+	/// Whether the window is to be dimmed.
+	enum tristate dim;
+	/// Whether this window should fade.
+	enum tristate fade;
+	/// Do not paint shadow over this window.
+	enum tristate clip_shadow_above;
+	/// Whether the window is to be considered focused.
+	enum tristate focused;
+	/// Whether the window is painted.
+	enum tristate paint;
+	/// Whether this window should be considered for unredirect-if-possible.
+	enum tristate unredir_ignore;
+};
+
+/// Like `window_maybe_options`, but all fields are guaranteed to be set.
+struct window_options {
+	double opacity;
+	const struct shader_info *shader;
+	unsigned int corner_radius;
+	bool transparent_clipping;
+	bool shadow;
+	bool invert_color;
+	bool blur_background;
+	bool dim;
+	bool fade;
+	bool clip_shadow_above;
+	bool focused;
+	bool paint;
+	bool unredir_ignore;
+
+	char padding[2];
+};
+
+static inline bool
+win_options_eq(const struct window_options *a, const struct window_options *b) {
+	return memcmp(a, b, offsetof(struct window_options, padding)) == 0;
+}
+
+static_assert(offsetof(struct window_options, padding) == 30, "window_options has "
+                                                              "implicit padding");
+
+extern struct shader_info null_shader;
 
 /// Structure representing all options.
 typedef struct options {

src/picom.c
@@ -751,15 +751,6 @@ static bool paint_preprocess(session_t *ps, bool *animation, struct win **out_bo
 		const winmode_t mode_old = w->mode;
 		const bool was_painted = w->to_paint;
 
-		if (win_should_dim(ps, w) != w->dim) {
-			w->dim = win_should_dim(ps, w);
-			add_damage_from_win(ps, w);
-		}
-
-		if (w->fg_shader && (w->fg_shader->attributes & SHADER_ATTRIBUTE_ANIMATED)) {
-			add_damage_from_win(ps, w);
-			*animation = true;
-		}
 		if (w->running_animation != NULL) {
 			*animation = true;
 		}
@@ -807,6 +798,11 @@ static bool paint_preprocess(session_t *ps, bool *animation, struct win **out_bo
 		const bool was_painted = w->to_paint;
 		const double window_opacity = win_animatable_get(w, WIN_SCRIPT_OPACITY);
 		const double blur_opacity = win_animatable_get(w, WIN_SCRIPT_BLUR_OPACITY);
+		auto window_options = win_options(w);
+		if (window_options.shader->attributes & SHADER_ATTRIBUTE_ANIMATED) {
+			add_damage_from_win(ps, w);
+			*animation = true;
+		}
 
 		// Destroy reg_ignore if some window above us invalidated it
 		if (!reg_ignore_valid) {
@@ -837,13 +833,14 @@ static bool paint_preprocess(session_t *ps, bool *animation, struct win **out_bo
 			log_trace("|- is positioned outside of the screen");
 			to_paint = false;
 		} else if (unlikely(window_opacity * MAX_ALPHA < 1 &&
-		                    (!w->blur_background || blur_opacity * MAX_ALPHA < 1))) {
+		                    (!window_options.blur_background ||
+		                     blur_opacity * MAX_ALPHA < 1))) {
 			// For consistency, even a window has 0 opacity, we would still
 			// blur its background. (unless it's background is not blurred, or
 			// the blur opacity is 0)
 			log_trace("|- has 0 opacity");
 			to_paint = false;
-		} else if (w->paint_excluded) {
+		} else if (!window_options.paint) {
 			log_trace("|- is excluded from painting");
 			to_paint = false;
 		} else if (unlikely((w->flags & WIN_FLAGS_PIXMAP_ERROR) != 0)) {
@@ -878,7 +875,7 @@ static bool paint_preprocess(session_t *ps, bool *animation, struct win **out_bo
 		// we add the window region to the ignored region
 		// Otherwise last_reg_ignore shouldn't change
 		if ((w->mode != WMODE_TRANS && !ps->o.force_win_blend) ||
-		    w->transparent_clipping) {
+		    window_options.transparent_clipping) {
 			// w->mode == WMODE_SOLID or WMODE_FRAME_TRANS
 			region_t *tmp = rc_region_new();
 			if (w->mode == WMODE_SOLID) {
@@ -903,7 +900,7 @@ static bool paint_preprocess(session_t *ps, bool *animation, struct win **out_bo
 		// is not correctly set.
 		if (ps->o.unredir_if_possible && is_highest) {
 			if (w->mode == WMODE_SOLID && !ps->o.force_win_blend &&
-			    w->is_fullscreen && !w->unredir_if_possible_excluded) {
+			    w->is_fullscreen && !window_options.unredir_ignore) {
 				unredir_possible = true;
 			}
 		}
@@ -911,7 +908,7 @@ static bool paint_preprocess(session_t *ps, bool *animation, struct win **out_bo
 		// Unredirect screen if some window is requesting compositor bypass, even
 		// if that window is not on the top.
 		if (ps->o.unredir_if_possible && win_is_bypassing_compositor(ps, w) &&
-		    !w->unredir_if_possible_excluded) {
+		    !window_options.unredir_ignore) {
 			// Here we deviate from EWMH a bit. EWMH says we must not
 			// unredirect the screen if the window requesting bypassing would
 			// look different after unredirecting. Instead we always follow
@@ -1543,7 +1540,7 @@ static void handle_new_window_attributes_reply(struct x_connection * /*c*/,
 	auto w = win_maybe_allocate(ps, toplevel,
 	                            (xcb_get_window_attributes_reply_t *)reply_or_error);
 	if (w != NULL && w->a.map_state == XCB_MAP_STATE_VIEWABLE) {
-		win_map_start(ps, w);
+		win_set_flags(w, WIN_FLAGS_MAPPED);
 		ps->pending_updates = true;
 	}
 }
@@ -1990,6 +1987,23 @@ static bool load_shader_source_for_condition(const c2_lptr_t *cond, void *data) 
 	return load_shader_source(data, c2_list_get_data(cond));
 }
 
+static struct window_options win_options_from_config(const struct options *opts) {
+	return (struct window_options){
+	    .blur_background = opts->blur_method != BLUR_METHOD_NONE,
+	    .shadow = opts->shadow_enable,
+	    .corner_radius = (unsigned)opts->corner_radius,
+	    .transparent_clipping = opts->transparent_clipping,
+	    .dim = opts->inactive_dim > 0,
+	    .fade = opts->fading_enable,
+	    .shader = &null_shader,
+	    .focused = false,
+	    .invert_color = false,
+	    .paint = true,
+	    .clip_shadow_above = false,
+	    .unredir_ignore = false,
+	};
+}
+
 /**
  * Initialize a session.
  *
@@ -2172,6 +2186,8 @@ static session_t *session_init(int argc, char **argv, Display *dpy,
 		          "invalid options.");
 		return NULL;
 	}
+
+	ps->window_options_default = win_options_from_config(&ps->o);
 
 	if (ps->o.window_shader_fg) {
 		log_debug("Default window shader: \"%s\"", ps->o.window_shader_fg);

src/renderer/command_builder.c
@@ -23,7 +23,7 @@ commands_for_window_body(struct layer *layer, struct backend_command *cmd,
 	auto mode = win_calc_mode_raw(layer->win);
 	int border_width = w->g.border_width;
 	double dim = 0;
-	if (w->dim) {
+	if (layer->options.dim) {
 		dim = inactive_dim;
 		if (!inactive_dim_fixed) {
 			dim *= layer->opacity;
@@ -48,7 +48,7 @@ commands_for_window_body(struct layer *layer, struct backend_command *cmd,
 			                         frame_region);
 		}
 	}
-	if (w->corner_radius > 0) {
+	if (layer->options.corner_radius > 0) {
 		win_region_remove_corners(w, layer->window.origin, &cmd->opaque_region);
 	}
 	region_scale(&cmd->target_mask, layer->window.origin, layer->scale);
@@ -61,15 +61,16 @@ commands_for_window_body(struct layer *layer, struct backend_command *cmd,
 	cmd->blit = (struct backend_blit_args){
 	    .border_width = border_width,
 	    .target_mask = &cmd->target_mask,
-	    .corner_radius = w->corner_radius,
+	    .corner_radius = layer->options.corner_radius,
 	    .opacity = layer->opacity,
 	    .dim = dim,
 	    .scale = layer->scale,
 	    .effective_size = layer->window.size,
-	    .shader = w->fg_shader ? w->fg_shader->backend_shader : NULL,
-	    .color_inverted = w->invert_color,
+	    .shader = layer->options.shader->backend_shader,
+	    .color_inverted = layer->options.invert_color,
 	    .source_mask = NULL,
-	    .max_brightness = max_brightness};
+	    .max_brightness = max_brightness,
+	};
 
 	if (w->frame_opacity == 1 || w->frame_opacity == 0) {
 		return 1;
@@ -97,7 +98,7 @@ command_for_shadow(struct layer *layer, struct backend_command *cmd,
                    const struct win_option *wintype_options,
                    const struct x_monitors *monitors, const struct backend_command *end) {
 	auto w = layer->win;
-	if (!w->shadow) {
+	if (!layer->options.shadow) {
 		return 0;
 	}
 
@@ -143,8 +144,8 @@ command_for_shadow(struct layer *layer, struct backend_command *cmd,
 		}
 	}
 	log_region(TRACE, &cmd->target_mask);
-	if (w->corner_radius > 0) {
-		cmd->source_mask.corner_radius = w->corner_radius;
+	if (layer->options.corner_radius > 0) {
+		cmd->source_mask.corner_radius = layer->options.corner_radius;
 		cmd->source_mask.inverted = true;
 		cmd->source_mask.origin =
 		    ivec2_sub(layer->window.origin, layer->shadow.origin);
@@ -156,7 +157,7 @@ command_for_shadow(struct layer *layer, struct backend_command *cmd,
 	cmd->blit = (struct backend_blit_args){
 	    .opacity = layer->shadow_opacity,
 	    .max_brightness = 1,
-	    .source_mask = w->corner_radius > 0 ? &cmd->source_mask : NULL,
+	    .source_mask = layer->options.corner_radius > 0 ? &cmd->source_mask : NULL,
 	    .scale = layer->shadow_scale,
 	    .effective_size = layer->shadow.size,
 	    .target_mask = &cmd->target_mask,
@@ -170,7 +171,7 @@ command_for_blur(struct layer *layer, struct backend_command *cmd,
                  const region_t *frame_region, bool force_blend, bool blur_frame) {
 	auto w = layer->win;
 	auto mode = win_calc_mode_raw(w);
-	if (!w->blur_background || layer->blur_opacity == 0) {
+	if (!layer->options.blur_background || layer->blur_opacity == 0) {
 		return 0;
 	}
 	if (force_blend || mode == WMODE_TRANS || layer->opacity < 1.0) {
@@ -189,15 +190,15 @@ command_for_blur(struct layer *layer, struct backend_command *cmd,
 
 	cmd->op = BACKEND_COMMAND_BLUR;
 	cmd->origin = (ivec2){};
-	if (w->corner_radius > 0) {
+	if (layer->options.corner_radius > 0) {
 		cmd->source_mask.origin = layer->window.origin;
-		cmd->source_mask.corner_radius = w->corner_radius;
+		cmd->source_mask.corner_radius = layer->options.corner_radius;
 		cmd->source_mask.inverted = false;
 	}
 	cmd->blur = (struct backend_blur_args){
 	    .opacity = layer->blur_opacity,
 	    .target_mask = &cmd->target_mask,
-	    .source_mask = w->corner_radius > 0 ? &cmd->source_mask : NULL,
+	    .source_mask = layer->options.corner_radius > 0 ? &cmd->source_mask : NULL,
 	};
 	return 1;
 }
@@ -217,7 +218,7 @@ command_builder_apply_transparent_clipping(struct layout *layout, region_t *scra
 	auto layer_start = end - layer->number_of_commands;
 	for (auto i = end; i != begin; i--) {
 		if (i == layer_start) {
-			if (layer->win->transparent_clipping) {
+			if (layer->options.transparent_clipping) {
 				auto win = layer->win;
 				auto mode = win_calc_mode_raw(layer->win);
 				region_t tmp;
@@ -263,7 +264,7 @@ command_builder_apply_shadow_clipping(struct layout *layout, region_t *scratch_r
 		if (i == layer_end) {
 			layer += 1;
 			layer_end += layer->number_of_commands;
-			clip_shadow_above = layer->win->clip_shadow_above;
+			clip_shadow_above = layer->options.clip_shadow_above;
 		}
 
 		if (i->op == BACKEND_COMMAND_BLUR) {
@@ -370,13 +371,13 @@ void command_builder_build(struct command_builder *cb, struct layout *layout, bo
 	unsigned ncmds = 1;
 	dynarr_foreach(layout->layers, layer) {
 		auto mode = win_calc_mode_raw(layer->win);
-		if (layer->win->blur_background && layer->blur_opacity > 0 &&
+		if (layer->options.blur_background && layer->blur_opacity > 0 &&
 		    (force_blend || mode == WMODE_TRANS || layer->opacity < 1.0 ||
 		     (blur_frame && mode == WMODE_FRAME_TRANS))) {
 			// Needs blur
 			ncmds += 1;
 		}
-		if (layer->win->shadow) {
+		if (layer->options.shadow) {
 			ncmds += 1;
 		}
 		if (layer->win->frame_opacity < 1 && layer->win->frame_opacity > 0) {

src/renderer/renderer.c
@@ -210,8 +210,9 @@ err:
 	return succeeded;
 }
 
-image_handle renderer_shadow_from_mask(struct renderer *r, struct backend_base *backend,
-                                       image_handle mask, int corner_radius, ivec2 mask_size) {
+image_handle
+renderer_shadow_from_mask(struct renderer *r, struct backend_base *backend,
+                          image_handle mask, unsigned int corner_radius, ivec2 mask_size) {
 	image_handle normalized_mask_image = NULL, shadow_image = NULL,
 	             shadow_color_pixel = NULL;
 	bool succeeded = false;
@@ -366,7 +367,7 @@ renderer_bind_shadow(struct renderer *r, struct backend_base *backend, struct wi
 			return false;
 		}
 		w->shadow_image = renderer_shadow_from_mask(
-		    r, backend, w->mask_image, w->corner_radius,
+		    r, backend, w->mask_image, win_options(w).corner_radius,
 		    (ivec2){.width = w->widthb, .height = w->heightb});
 	}
 	if (!w->shadow_image) {

commit 636cb5b792cd57d4f930286bd6ae9676009c53a4

wm/win: fix window-shader-fg/window-shader-fg-rules not working
Fixes: cdf25b07dc8dd41985c8987df793f4d17ebb5261
Signed-off-by: Yuxuan Shui <yshuiv7@gmail.com>

src/config.h
@@ -173,8 +173,8 @@ struct window_maybe_options {
 	/// Window dim level, NaN means not set.
 	double dim;
 
-	/// Name of the custom fragment shader for this window. NULL means not set.
-	const struct shader_info *shader;
+	/// The name of the custom fragment shader for this window. NULL means not set.
+	const char *shader;
 
 	/// Whether transparent clipping is excluded by the rules.
 	enum tristate transparent_clipping;
@@ -206,7 +206,7 @@ struct window_maybe_options {
 struct window_options {
 	double opacity;
 	double dim;
-	const struct shader_info *shader;
+	const char *shader;
 	unsigned int corner_radius;
 	enum window_unredir_option unredir;
 	bool transparent_clipping;

src/options.c
@@ -1054,6 +1054,12 @@ void options_postprocess_c2_lists(struct c2_state *state, struct x_connection *c
 	}
 }
 
+static void free_window_maybe_options(void *data) {
+	auto wopts = (struct window_maybe_options *)data;
+	free((void *)wopts->shader);
+	free(wopts);
+}
+
 void options_destroy(struct options *options) {
 	// Free blacklists
 	c2_list_free(&options->shadow_blacklist, NULL);
@@ -1069,7 +1075,7 @@ void options_destroy(struct options *options) {
 	c2_list_free(&options->corner_radius_rules, NULL);
 	c2_list_free(&options->window_shader_fg_rules, free);
 	c2_list_free(&options->transparent_clipping_blacklist, NULL);
-	c2_list_free(&options->rules, free);
+	c2_list_free(&options->rules, free_window_maybe_options);
 
 	free(options->config_file_path);
 	free(options->write_pid_path);

src/picom.c
@@ -799,7 +799,11 @@ static bool paint_preprocess(session_t *ps, bool *animation, struct win **out_bo
 		const double window_opacity = win_animatable_get(w, WIN_SCRIPT_OPACITY);
 		const double blur_opacity = win_animatable_get(w, WIN_SCRIPT_BLUR_OPACITY);
 		auto window_options = win_options(w);
-		if (window_options.shader->attributes & SHADER_ATTRIBUTE_ANIMATED) {
+		struct shader_info *fg_shader = NULL;
+		if (window_options.shader != NULL) {
+			HASH_FIND_STR(ps->shaders, window_options.shader, fg_shader);
+		}
+		if (fg_shader != NULL && fg_shader->attributes & SHADER_ATTRIBUTE_ANIMATED) {
 			add_damage_from_win(ps, w);
 			*animation = true;
 		}
@@ -1852,7 +1856,7 @@ static void draw_callback_impl(EV_P_ session_t *ps, int revents attr_unused) {
 			    ps->o.force_win_blend, ps->o.blur_background_frame,
 			    ps->o.inactive_dim_fixed, ps->o.max_brightness,
 			    ps->o.crop_shadow_to_monitor ? &ps->monitors : NULL,
-			    &after_damage_us);
+			    ps->shaders, &after_damage_us);
 			if (!succeeded) {
 				log_fatal("Render failure");
 				abort();
@@ -2000,7 +2004,7 @@ static struct window_options win_options_from_config(const struct options *opts)
 	    .transparent_clipping = opts->transparent_clipping,
 	    .dim = opts->inactive_dim > 0,
 	    .fade = opts->fading_enable,
-	    .shader = &null_shader,
+	    .shader = opts->window_shader_fg,
 	    .invert_color = false,
 	    .paint = true,
 	    .clip_shadow_above = false,

src/renderer/command_builder.c
@@ -17,7 +17,7 @@
 static inline unsigned
 commands_for_window_body(struct layer *layer, struct backend_command *cmd_base,
                          const region_t *frame_region, bool inactive_dim_fixed,
-                         double max_brightness) {
+                         double max_brightness, const struct shader_info *shaders) {
 	auto w = layer->win;
 	auto cmd = cmd_base;
 	scoped_region_t crop = region_from_box(layer->crop);
@@ -49,6 +49,10 @@ commands_for_window_body(struct layer *layer, struct backend_command *cmd_base,
 	if (layer->options.corner_radius > 0) {
 		win_region_remove_corners(w, layer->window.origin, &cmd->opaque_region);
 	}
+	struct shader_info *shader = &null_shader;
+	if (layer->options.shader != NULL) {
+		HASH_FIND_STR(shaders, layer->options.shader, shader);
+	}
 
 	float opacity = layer->opacity * (1 - layer->saved_image_blend);
 	if (opacity > (1. - 1. / MAX_ALPHA)) {
@@ -66,7 +70,7 @@ commands_for_window_body(struct layer *layer, struct backend_command *cmd_base,
 	    .dim = dim,
 	    .scale = layer->scale,
 	    .effective_size = layer->window.size,
-	    .shader = layer->options.shader->backend_shader,
+	    .shader = shader->backend_shader,
 	    .color_inverted = layer->options.invert_color,
 	    .source_mask = NULL,
 	    .max_brightness = max_brightness,
@@ -408,7 +412,8 @@ void command_builder_free(struct command_builder *cb) {
 // value in `struct managed_win`.
 void command_builder_build(struct command_builder *cb, struct layout *layout,
                            bool force_blend, bool blur_frame, bool inactive_dim_fixed,
-                           double max_brightness, const struct x_monitors *monitors) {
+                           double max_brightness, const struct x_monitors *monitors,
+                           const struct shader_info *shaders) {
 
 	unsigned ncmds = 1;
 	dynarr_foreach(layout->layers, layer) {
@@ -445,8 +450,8 @@ void command_builder_build(struct command_builder *cb, struct layout *layout,
 		                          layer->window.origin.y);
 
 		// Add window body
-		cmd -= commands_for_window_body(layer, cmd, &frame_region,
-		                                inactive_dim_fixed, max_brightness);
+		cmd -= commands_for_window_body(
+		    layer, cmd, &frame_region, inactive_dim_fixed, max_brightness, shaders);
 
 		// Add shadow
 		cmd -= command_for_shadow(layer, cmd, monitors, last + 1);

src/renderer/command_builder.h
@@ -10,6 +10,7 @@ struct backend_command;
 struct layout;
 struct x_monitors;
 struct win_option;
+struct shader_info;
 
 struct command_builder *command_builder_new(void);
 void command_builder_free(struct command_builder *);
@@ -25,4 +26,5 @@ void command_builder_command_list_free(struct backend_command *cmds);
 /// stay true after further passes.
 void command_builder_build(struct command_builder *cb, struct layout *layout,
                            bool force_blend, bool blur_frame, bool inactive_dim_fixed,
-                           double max_brightness, const struct x_monitors *monitors);
+                           double max_brightness, const struct x_monitors *monitors,
+                           const struct shader_info *shaders);

src/renderer/renderer.c
@@ -489,11 +489,11 @@ void renderer_ensure_images_ready(struct renderer *r, struct backend_base *backe
 /// @return true if a frame is rendered, false if this frame is skipped.
 bool renderer_render(struct renderer *r, struct backend_base *backend,
                      image_handle root_image, struct layout_manager *lm,
-                     struct command_builder *cb, void *blur_context,
-                     uint64_t render_start_us, xcb_sync_fence_t xsync_fence,
-                     bool use_damage, bool monitor_repaint, bool force_blend,
-                     bool blur_frame, bool inactive_dim_fixed, double max_brightness,
-                     const struct x_monitors *monitors, uint64_t *after_damage_us) {
+                     struct command_builder *cb, void *blur_context, uint64_t render_start_us,
+                     xcb_sync_fence_t xsync_fence, bool use_damage, bool monitor_repaint,
+                     bool force_blend, bool blur_frame, bool inactive_dim_fixed,
+                     double max_brightness, const struct x_monitors *monitors,
+                     const struct shader_info *shaders, uint64_t *after_damage_us) {
 	if (xsync_fence != XCB_NONE) {
 		// Trigger the fence but don't immediately wait on it. Let it run
 		// concurrent with our CPU tasks to save time.
@@ -514,7 +514,7 @@ bool renderer_render(struct renderer *r, struct backend_base *backend,
 	renderer_ensure_images_ready(r, backend, monitor_repaint);
 
 	command_builder_build(cb, layout, force_blend, blur_frame, inactive_dim_fixed,
-	                      max_brightness, monitors);
+	                      max_brightness, monitors, shaders);
 	if (log_get_level_tls() <= LOG_LEVEL_TRACE) {
 		auto layer = layout->layers - 1;
 		auto layer_end = &layout->commands[layout->first_layer_start];

src/renderer/renderer.h
@@ -11,6 +11,7 @@ struct renderer;
 struct layout_manager;
 struct backend_base;
 struct command_builder;
+struct shader_info;
 typedef struct image_handle *image_handle;
 struct x_monitors;
 struct wm;
@@ -22,8 +23,8 @@ struct renderer *renderer_new(struct backend_base *backend, double shadow_radius
                               struct color shadow_color, bool dithered_present);
 bool renderer_render(struct renderer *r, struct backend_base *backend,
                      image_handle root_image, struct layout_manager *lm,
-                     struct command_builder *cb, void *blur_context,
-                     uint64_t render_start_us, xcb_sync_fence_t xsync_fence,
-                     bool use_damage, bool monitor_repaint, bool force_blend,
-                     bool blur_frame, bool inactive_dim_fixed, double max_brightness,
-                     const struct x_monitors *monitors, uint64_t *after_damage_us);
+                     struct command_builder *cb, void *blur_context, uint64_t render_start_us,
+                     xcb_sync_fence_t xsync_fence, bool use_damage, bool monitor_repaint,
+                     bool force_blend, bool blur_frame, bool inactive_dim_fixed,
+                     double max_brightness, const struct x_monitors *monitors,
+                     const struct shader_info *shaders, uint64_t *after_damage_us);

src/wm/win.c
@@ -980,12 +980,11 @@ static void win_determine_fg_shader(session_t *ps, struct win *w) {
 		return;
 	}
 
-	void *val = NULL;
 	w->options.shader = NULL;
+
+	void *val = NULL;
 	if (c2_match(ps->c2_state, w, &ps->o.window_shader_fg_rules, &val)) {
-		struct shader_info *shader = NULL;
-		HASH_FIND_STR(ps->shaders, val, shader);
-		w->options.shader = shader;
+		w->options.shader = val;
 	}
 }
 