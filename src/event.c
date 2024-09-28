57a16f5bcb22bbc4ca57e463e9998fcc2d78e7c6
Suyooo <me@suyo.be>
2024-09-28 11:02:04 +0200

    core: Fix root configure event being ignored
    The guard added in commit 0aa620 to avoid duplicates filters out all Configure events on the root window, causing picom
    to not properly render the screen area after display configuration changes, such as adding or resizing a monitor.
    
    Only apply the guard if the Configure event affects a window other than the root window.
    
    Fixes #1338
    
    (cherry picked from commit 16e17df1a157a4cd9d25d1b932b74884dbbb6ff9)

--- src/event.c
+++ src/event.c
@@ -388,13 +388,13 @@ static inline void ev_configure_notify(session_t *ps, xcb_configure_notify_event
 		return;
 	}
 
-	if (ev->window == ev->event) {
-		return;
-	}
-
 	if (ev->window == ps->c.screen_info->root) {
 		configure_root(ps);
 	} else {
+		if (ev->window == ev->event) {
+			return;
+		}
+
 		configure_win(ps, ev);
 	}
 }
