1e56feeeb97baba43dc1e09ae2e135ea0f90eeb1
Maxim Solovyov <msolovyov@protonmail.com>
2025-02-21 16:19:37 +0300

    x: use XSyncBad{Counter,Alarm,Fence} from X11/extensions/syncconst.h

--- src/x.c
+++ src/x.c
@@ -7,6 +7,7 @@
 
 #include <X11/Xlib-xcb.h>
 #include <X11/Xutil.h>
+#include <X11/extensions/syncconst.h>
 #include <pixman.h>
 #include <xcb/composite.h>
 #include <xcb/damage.h>
@@ -40,12 +41,6 @@ static inline uint64_t x_widen_sequence(struct x_connection *c, uint32_t sequenc
 }
 
 // === Error handling ===
-
-enum {
-	XSyncBadCounter = 0,
-	XSyncBadAlarm = 1,
-	XSyncBadFence = 2,
-};
 
 /// Convert a X11 error to string
 ///
