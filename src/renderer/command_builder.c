yshui/picom

commit 4275403f7244436591faacd6b90a3ee57de2465f

renderer, backend: refactor how shadows are rendered
This commit touches a couple of areas, and unfortunately cannot be broken into
smaller ones since the changes all depends on each other.

To start, a short explanation of how shadows are rendered now. Currently, each
window gets a pre-generated shadow image, this shadow image has shadow-color and
shadow-opacity baked in. Which means changing color or opacity requires
re-generating the shadow image (caveat on shadow-opacity, because it is applied
twice, we can still apply a opacity on the generated shadow image, but that is
wrong and is a bug).

We want to have dynamic shadow colors, which means this has to change.

Firstly, the `opacity` and `dim` parameters for the blit backend operations are
replaced with `tint`, which is a generalization of both parameters. This can be
used to multiply each of the color channels by a separate factor.

Next, we change how shadows are generated. Instead of a color image, we instead
generate a single channel mask image. This is done by mostly removing color,
since what we are doing right now is generating a mask then coloring it, so we
just remove the coloring step. This is fine if the backend support the "blurring
a mask" codepath. But xrender doesn't have that, and is using the "generating a
pixmap -> backend->ops.bind_pixmap" path. This is problematic because
`bind_pixmap` needs a X VisualID, but single channel masks don't have one. This
is why we added a new `new_image_from_pixels` API for uploading CPU pixel bytes
directly into backend images, bypassing `bind_pixmap`. And using it also means
we are no longer limited by the maximum allowed X request size.

Finally, we change how shadows are rendered, by combining the result of previous
two steps - we apply a `tint` to the shadow mask and Voila.

P.S. Implementing tinting in the GL backend is simple, and actually simplifies
the shader code. Only problems is removing `opacity` and `dim` uniforms will

src/renderer/command_builder.c
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MPL-2.0
 // Copyright (c) Yuxuan Shui <yshuiv7@gmail.com>
 
+#include <picom/types.h>
+
 #include "backend/backend.h"
 #include "common.h"
 #include "layout.h"
@@ -68,8 +70,9 @@ commands_for_window_body(struct layer *layer, struct backend_command *cmd_base,
 	struct backend_blit_args args_base = {
 	    .border_width = border_width,
 	    .corner_radius = layer->options.corner_radius,
-	    .opacity = opacity,
-	    .dim = dim,
+	    .tint = color_mult_alpha(
+	        (struct color){.red = 1. - dim, .green = 1. - dim, .blue = 1. - dim, .alpha = 1.},
+	        opacity),
 	    .scale = layer->scale,
 	    .effective_size = layer->window.size,
 	    .shader = shader != NULL ? shader->backend_shader : NULL,
@@ -99,7 +102,7 @@ commands_for_window_body(struct layer *layer, struct backend_command *cmd_base,
 		    .width = (int)(layer->window.size.width / w->saved_win_image_scale.width),
 		    .height = (int)(layer->window.size.height / w->saved_win_image_scale.height),
 		};
-		cmd->blit.opacity = opacity_saved;
+		cmd->blit.tint = color_mult_alpha(cmd->blit.tint, opacity_saved);
 		cmd->blit.target_mask = &cmd->target_mask;
 		cmd->blit.scale = vec2_scale(cmd->blit.scale, w->saved_win_image_scale);
 		cmd -= 1;
@@ -118,7 +121,7 @@ commands_for_window_body(struct layer *layer, struct backend_command *cmd_base,
 	cmd->source = BACKEND_COMMAND_SOURCE_WINDOW;
 	cmd->blit = args_base;
 	cmd->blit.target_mask = &cmd->target_mask;
-	cmd->blit.opacity = w->frame_opacity * opacity;
+	cmd->blit.tint = color_mult_alpha(cmd->blit.tint, w->frame_opacity * opacity);
 	cmd -= 1;
 	if (layer->saved_image_blend > 0) {
 		pixman_region32_copy(&cmd->target_mask, &cmd[1].target_mask);
@@ -131,7 +134,7 @@ commands_for_window_body(struct layer *layer, struct backend_command *cmd_base,
 		    .width = (int)(layer->window.size.width / w->saved_win_image_scale.width),
 		    .height = (int)(layer->window.size.height / w->saved_win_image_scale.height),
 		};
-		cmd->blit.opacity = w->frame_opacity * opacity_saved;
+		cmd->blit.tint = color_mult_alpha(cmd->blit.tint, w->frame_opacity * opacity);
 		cmd->blit.target_mask = &cmd->target_mask;
 		cmd->blit.scale = vec2_scale(cmd->blit.scale, w->saved_win_image_scale);
 		cmd -= 1;
@@ -203,8 +206,10 @@ command_for_shadow(struct layer *layer, struct backend_command *cmd,
 	scoped_region_t crop = region_from_box(layer->crop);
 	pixman_region32_intersect(&cmd->target_mask, &cmd->target_mask, &crop);
 
+	auto color = layer->win->shadow_color;
+	color = color_mult_alpha(color, layer->shadow_opacity);
 	cmd->blit = (struct backend_blit_args){
-	    .opacity = layer->shadow_opacity,
+	    .tint = color,
 	    .max_brightness = 1,
 	    .source_mask = layer->options.corner_radius > 0 ? &cmd->source_mask : NULL,
 	    .scale = layer->shadow_scale,

include/picom/backend.h
@@ -124,10 +124,12 @@ struct backend_blit_args {
 	const region_t *target_mask;
 	/// Custom shader for this blit operation.
 	void *shader;
-	/// Opacity of the source image.
-	double opacity;
-	/// Dim level of the source image.
-	double dim;
+	/// Tint. Multiply each color channel by a specific factor. i.e.
+	/// out.c = in.c * tint.c, where c = r, g, b, or a.
+	///
+	/// Note since the backends operate in pre-mult alpha mode, applying
+	/// a factor to alpha requires applying the same factor to other colors too.
+	struct color tint;
 	/// Brightness limit of the source image. Source image
 	/// will be normalized so that the maximum brightness is
 	/// this value.

include/picom/types.h
@@ -53,6 +53,19 @@ struct color {
 	double red, green, blue, alpha;
 };
 
+static inline bool color_eq(struct color a, struct color b) {
+	return a.red == b.red && a.green == b.green && a.blue == b.blue && a.alpha == b.alpha;
+}
+
+static inline struct color color_mult_alpha(struct color a, double alpha) {
+	return (struct color){
+	    .red = a.red * alpha,
+	    .green = a.green * alpha,
+	    .blue = a.blue * alpha,
+	    .alpha = a.alpha * alpha,
+	};
+}
+
 typedef uint32_t opacity_t;
 
 typedef struct vec2 {
@@ -188,5 +201,4 @@ static inline ivec2 ivec2_scale_floor(ivec2 a, vec2 scale) {
 	return vec2_as(vec2_floor(scaled));
 }
 
-#define MARGIN_INIT                                                                      \
-	{ 0, 0, 0, 0 }
+#define MARGIN_INIT {0, 0, 0, 0}

src/backend/backend.c
@@ -93,7 +93,7 @@ bool backend_execute(struct backend_base *backend, image_handle target, unsigned
 			if (!pixman_region32_not_empty(cmd->blit.target_mask)) {
 				continue;
 			}
-			if (cmd->blit.opacity < 1. / MAX_ALPHA) {
+			if (cmd->blit.tint.alpha < 1. / MAX_ALPHA) {
 				continue;
 			}
 			succeeded =

src/backend/backend.h
@@ -45,8 +45,9 @@ struct backend_command {
 		struct backend_blur_args blur;
 	};
 	/// Source mask for the operation.
-	/// If the `source_mask` of the operation's argument points to this, a mask image
-	/// will be created for the operation for the renderer.
+	/// This may be used for the `source_mask` field of `op` by the command builder.
+	/// `image` of `source_mask` is usually left empty by the command builder, and
+	/// the renderer will fill it in appropriately later.
 	struct backend_mask_image source_mask;
 	/// Target mask for the operation.
 	region_t target_mask;

src/backend/backend_common.c
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MPL-2.0
 // Copyright (c) Yuxuan Shui <yshuiv7@gmail.com>
 #include <math.h>
+#include <stddef.h>
+#include <stdint.h>
 #include <string.h>
 #include <xcb/render.h>
 #include <xcb/xcb_image.h>
@@ -32,7 +34,7 @@ solid_picture(struct x_connection *c, bool argb, double a, double r, double g, d
 		return XCB_NONE;
 	}
 
-	pa.repeat = 1;
+	pa.repeat = XCB_RENDER_REPEAT_NORMAL;
 	picture = x_create_picture_with_standard_and_pixmap(
 	    c, argb ? XCB_PICT_STANDARD_ARGB_32 : XCB_PICT_STANDARD_A_8, pixmap,
 	    XCB_RENDER_CP_REPEAT, &pa);
@@ -58,8 +60,8 @@ solid_picture(struct x_connection *c, bool argb, double a, double r, double g, d
 	return picture;
 }
 
-xcb_image_t *make_shadow(struct x_connection *c, const conv *kernel, double opacity,
-                         int width, int height) {
+uint8_t *make_shadow(struct x_connection *c, const conv *kernel, ivec2 window_size,
+                     ivec2 *out_shadow_size, int *out_shadow_stride) {
 	/*
 	 * We classify shadows into 4 kinds of regions
 	 *    r = shadow radius
@@ -73,11 +75,11 @@ xcb_image_t *make_shadow(struct x_connection *c, const conv *kernel, double opac
 	 *          |  1  |    2    |  1  |
 	 * height+r +-----+---------+-----+
 	 */
-	xcb_image_t *ximage;
 	const double *shadow_sum = kernel->rsum;
 	assert(shadow_sum);
 	// We only support square kernels for shadow
 	assert(kernel->w == kernel->h);
+	int width = window_size.width, height = window_size.height;
 	int d = kernel->w;
 	int r = d / 2;
 	int swidth = width + r * 2, sheight = height + r * 2;
@@ -85,16 +87,32 @@ xcb_image_t *make_shadow(struct x_connection *c, const conv *kernel, double opac
 	assert(d % 2 == 1);
 	assert(d > 0);
 
-	ximage =
-	    xcb_image_create_native(c->c, to_u16_checked(swidth), to_u16_checked(sheight),
-	                            XCB_IMAGE_FORMAT_Z_PIXMAP, 8, 0, 0, NULL);
-	if (!ximage) {
-		log_error("failed to create an X image");
-		return 0;
+	int scanline_pad = 0;
+	auto setup = xcb_get_setup(c->c);
+	xcb_format_t *fmt = xcb_setup_pixmap_formats(setup);
+	xcb_format_t *fmtend = fmt + xcb_setup_pixmap_formats_length(setup);
+	for (; fmt != fmtend; ++fmt) {
+		if (fmt->depth == 8) {
+			scanline_pad = fmt->scanline_pad;
+			break;
+		}
+	}
+	if (scanline_pad == 0) {
+		log_error("Couldn't find valid pixmap format with depth 8.");
+		return NULL;
+	}
+	scanline_pad = scanline_pad / 8 - 1;
+	int sstride = (swidth + scanline_pad) & (~scanline_pad);
+
+	uint8_t *data = calloc((size_t)sheight, (size_t)sstride);
+	if (!data) {
+		log_error("failed to allocate memory for shadow mask");
+		return NULL;
 	}
 
-	unsigned char *data = ximage->data;
-	long long sstride = ximage->stride;
+	out_shadow_size->width = swidth;
+	out_shadow_size->height = sheight;
+	*out_shadow_stride = sstride;
 
 	// If the window body is smaller than the kernel, we do convolution directly
 	if (width < r * 2 && height < r * 2) {
@@ -102,10 +120,10 @@ xcb_image_t *make_shadow(struct x_connection *c, const conv *kernel, double opac
 			for (int x = 0; x < swidth; x++) {
 				double sum = sum_kernel_normalized(
 				    kernel, d - x - 1, d - y - 1, width, height);
-				data[y * sstride + x] = (uint8_t)(sum * 255.0 * opacity);
+				data[y * sstride + x] = (uint8_t)(sum * 255.0);
 			}
 		}
-		return ximage;
+		return data;
 	}
 
 	if (height < r * 2) {
@@ -120,18 +138,18 @@ xcb_image_t *make_shadow(struct x_connection *c, const conv *kernel, double opac
 			for (int x = 0; x < r * 2; x++) {
 				double sum = sum_kernel_normalized(kernel, d - x - 1,
 				                                   d - y - 1, d, height) *
-				             255.0 * opacity;
+				             255.0;
 				data[y * sstride + x] = (uint8_t)sum;
 				data[y * sstride + swidth - x - 1] = (uint8_t)sum;
 			}
 		}
 		for (int y = 0; y < sheight; y++) {
-			double sum = sum_kernel_normalized(kernel, 0, d - y - 1, d, height) *
-			             255.0 * opacity;
+			double sum =
+			    sum_kernel_normalized(kernel, 0, d - y - 1, d, height) * 255.0;
 			memset(&data[y * sstride + r * 2], (uint8_t)sum,
 			       (size_t)(width - 2 * r));
 		}
-		return ximage;
+		return data;
 	}
 	if (width < r * 2) {
 		// Similarly, for width smaller than kernel
@@ -139,32 +157,32 @@ xcb_image_t *make_shadow(struct x_connection *c, const conv *kernel, double opac
 			for (int x = 0; x < swidth; x++) {
 				double sum = sum_kernel_normalized(kernel, d - x - 1,
 				                                   d - y - 1, width, d) *
-				             255.0 * opacity;
+				             255.0;
 				data[y * sstride + x] = (uint8_t)sum;
 				data[(sheight - y - 1) * sstride + x] = (uint8_t)sum;
 			}
 		}
 		for (int x = 0; x < swidth; x++) {
-			double sum = sum_kernel_normalized(kernel, d - x - 1, 0, width, d) *
-			             255.0 * opacity;
+			double sum =
+			    sum_kernel_normalized(kernel, d - x - 1, 0, width, d) * 255.0;
 			for (int y = r * 2; y < height; y++) {
 				data[y * sstride + x] = (uint8_t)sum;
 			}
 		}
-		return ximage;
+		return data;
 	}
 
 	// Implies: width >= r * 2 && height >= r * 2
 
 	// Fill part 3
 	for (int y = r; y < height + r; y++) {
-		memset(data + sstride * y + r, (uint8_t)(255 * opacity), (size_t)width);
+		memset(data + (ptrdiff_t)sstride * y + r, 0xff, (size_t)width);
 	}
 
 	// Part 1
 	for (int y = 0; y < r * 2; y++) {
 		for (int x = 0; x < r * 2; x++) {
-			double tmpsum = shadow_sum[y * d + x] * opacity * 255.0;
+			double tmpsum = shadow_sum[y * d + x] * 255.0;
 			data[y * sstride + x] = (uint8_t)tmpsum;
 			data[(sheight - y - 1) * sstride + x] = (uint8_t)tmpsum;
 			data[(sheight - y - 1) * sstride + (swidth - x - 1)] = (uint8_t)tmpsum;
@@ -174,7 +192,7 @@ xcb_image_t *make_shadow(struct x_connection *c, const conv *kernel, double opac
 
 	// Part 2, top/bottom
 	for (int y = 0; y < r * 2; y++) {
-		double tmpsum = shadow_sum[d * y + d - 1] * opacity * 255.0;
+		double tmpsum = shadow_sum[d * y + d - 1] * 255.0;
 		memset(&data[y * sstride + r * 2], (uint8_t)tmpsum, (size_t)(width - r * 2));
 		memset(&data[(sheight - y - 1) * sstride + r * 2], (uint8_t)tmpsum,
 		       (size_t)(width - r * 2));
@@ -182,116 +200,14 @@ xcb_image_t *make_shadow(struct x_connection *c, const conv *kernel, double opac
 
 	// Part 2, left/right
 	for (int x = 0; x < r * 2; x++) {
-		double tmpsum = shadow_sum[d * (d - 1) + x] * opacity * 255.0;
+		double tmpsum = shadow_sum[d * (d - 1) + x] * 255.0;
 		for (int y = r * 2; y < height; y++) {
 			data[y * sstride + x] = (uint8_t)tmpsum;
 			data[y * sstride + (swidth - x - 1)] = (uint8_t)tmpsum;
 		}
 	}
 
-	return ximage;
-}
-
-/**
- * Generate shadow <code>Picture</code> for a window.
- */
-bool build_shadow(struct x_connection *c, double opacity, const int width,
-                  const int height, const conv *kernel, xcb_render_picture_t shadow_pixel,
-                  xcb_pixmap_t *pixmap) {
-	xcb_image_t *shadow_image = NULL;
-	xcb_pixmap_t shadow_pixmap = XCB_NONE, shadow_pixmap_argb = XCB_NONE;
-	xcb_render_picture_t shadow_picture = XCB_NONE, shadow_picture_argb = XCB_NONE;
-	xcb_gcontext_t gc = XCB_NONE;
-
-	shadow_image = make_shadow(c, kernel, opacity, width, height);
-	if (!shadow_image) {
-		log_error("Failed to make shadow");
-		return false;
-	}
-
-	shadow_pixmap = x_create_pixmap(c, 8, shadow_image->width, shadow_image->height);
-	shadow_pixmap_argb =
-	    x_create_pixmap(c, 32, shadow_image->width, shadow_image->height);
-
-	if (!shadow_pixmap || !shadow_pixmap_argb) {
-		log_error("Failed to create shadow pixmaps");
-		goto shadow_picture_err;
-	}
-
-	shadow_picture = x_create_picture_with_standard_and_pixmap(
-	    c, XCB_PICT_STANDARD_A_8, shadow_pixmap, 0, NULL);
-	shadow_picture_argb = x_create_picture_with_standard_and_pixmap(
-	    c, XCB_PICT_STANDARD_ARGB_32, shadow_pixmap_argb, 0, NULL);
-	if (!shadow_picture || !shadow_picture_argb) {
-		goto shadow_picture_err;
-	}
-
-	gc = x_new_id(c);
-	xcb_create_gc(c->c, gc, shadow_pixmap, 0, NULL);
-
-	// We need to make room for protocol metadata in the request. The metadata should
-	// be 24 bytes plus padding, let's be generous and give it 1kb
-	auto maximum_image_size = xcb_get_maximum_request_length(c->c) * 4 - 1024;
-	auto maximum_row =
-	    to_u16_checked(clamp(maximum_image_size / shadow_image->stride, 0, UINT16_MAX));
-	if (maximum_row <= 0) {
-		// TODO(yshui) Upload image with XShm
-		log_error("X server request size limit is too restrictive, or the shadow "
-		          "image is too wide for us to send a single row of the shadow "
-		          "image. Shadow size: %dx%d",
-		          width, height);
-		goto shadow_picture_err;
-	}
-
-	for (uint32_t row = 0; row < shadow_image->height; row += maximum_row) {
-		auto batch_height = maximum_row;
-		if (batch_height > shadow_image->height - row) {
-			batch_height = to_u16_checked(shadow_image->height - row);
-		}
-
-		auto offset =
-		    (size_t)row * shadow_image->stride / sizeof(*shadow_image->data);
-		xcb_put_image(c->c, (uint8_t)shadow_image->format, shadow_pixmap, gc,
-		              shadow_image->width, batch_height, 0, to_i16_checked(row),
-		              0, shadow_image->depth, shadow_image->stride * batch_height,
-		              shadow_image->data + offset);
-	}
-
-	xcb_render_composite(c->c, XCB_RENDER_PICT_OP_SRC, shadow_pixel, shadow_picture,
-	                     shadow_picture_argb, 0, 0, 0, 0, 0, 0, shadow_image->width,
-	                     shadow_image->height);
-
-	*pixmap = shadow_pixmap_argb;
-
-	xcb_free_gc(c->c, gc);
-	xcb_image_destroy(shadow_image);
-	xcb_free_pixmap(c->c, shadow_pixmap);
-	x_free_picture(c, shadow_picture);
-	x_free_picture(c, shadow_picture_argb);
-
-	return true;
-
-shadow_picture_err:
-	if (shadow_image) {
-		xcb_image_destroy(shadow_image);
-	}
-	if (shadow_pixmap) {
-		xcb_free_pixmap(c->c, shadow_pixmap);
-	}
-	if (shadow_pixmap_argb) {
-		xcb_free_pixmap(c->c, shadow_pixmap_argb);
-	}
-	if (shadow_picture) {
-		x_free_picture(c, shadow_picture);
-	}
-	if (shadow_picture_argb) {
-		x_free_picture(c, shadow_picture_argb);
-	}
-	if (gc) {
-		xcb_free_gc(c->c, gc);
-	}
-
-	return false;
+	return data;
 }
 
 static struct conv **generate_box_blur_kernel(struct box_blur_args *args, int *kernel_count) {

src/backend/backend_common.h
@@ -25,10 +25,8 @@ struct dual_kawase_params {
 	int expand;
 };
 
-xcb_image_t *make_shadow(struct x_connection *c, const conv *kernel, double opacity,
-                         int width, int height);
-bool build_shadow(struct x_connection *, double opacity, int width, int height,
-                  const conv *kernel, xcb_render_picture_t shadow_pixel, xcb_pixmap_t *pixmap);
+uint8_t *make_shadow(struct x_connection *c, const conv *kernel, ivec2 window_size,
+                     ivec2 *out_shadow_size, int *out_shadow_stride);
 
 xcb_render_picture_t
 solid_picture(struct x_connection *, bool argb, double a, double r, double g, double b);

src/backend/gl/gl_common.c
@@ -560,18 +560,22 @@ gl_lower_blit_args(struct gl_data *gd, ivec2 origin, const struct backend_blit_a
 	if (border_width > args->corner_radius) {
 		border_width = 0;
 	}
+	double dim_compat = 1.0 - (args->tint.red + args->tint.green + args->tint.blue) / 3.0;
 	// clang-format off
 	auto tex_sampler = vec2_eq(args->scale, SCALE_IDENTITY) ?
 	    gd->samplers[GL_SAMPLER_REPEAT] : gd->samplers[GL_SAMPLER_REPEAT_SCALE];
 	struct gl_uniform_value from_uniforms[] = {
-	    [UNIFORM_OPACITY_LOC]        = {.type = GL_FLOAT, .f = (float)args->opacity},
+	    [UNIFORM_OPACITY_LOC]        = {.type = GL_FLOAT, .f = (float)args->tint.alpha},
 	    [UNIFORM_INVERT_COLOR_LOC]   = {.type = GL_INT, .i = args->color_inverted},
 	    [UNIFORM_TEX_LOC]            = {.type = GL_TEXTURE_2D,
 	                                    .tu = {img->texture, tex_sampler}},
 	    [UNIFORM_EFFECTIVE_SIZE_LOC] = {.type = GL_FLOAT_VEC2,
 	                                    .f2 = {(float)args->effective_size.width,
-					           (float)args->effective_size.height}},
-	    [UNIFORM_DIM_LOC]            = {.type = GL_FLOAT, .f = (float)args->dim},
+	                                           (float)args->effective_size.height}},
+	    [UNIFORM_DIM_LOC]            = {.type = GL_FLOAT, .f = (float)dim_compat},
+	    [UNIFORM_TINT_LOC]           = {.type = GL_FLOAT_VEC4,
+	                                    .f4 = {(float)args->tint.red, (float)args->tint.green,
+	                                           (float)args->tint.blue, (float)args->tint.alpha}},
 	    [UNIFORM_BRIGHTNESS_LOC]     = {.type = GL_TEXTURE_2D,
 	                                    .tu = {brightness, gd->samplers[GL_SAMPLER_EDGE]}},
 	    [UNIFORM_MAX_BRIGHTNESS_LOC] = {.type = GL_FLOAT, .f = (float)args->max_brightness},
@@ -809,7 +813,7 @@ static inline void gl_init_uniform_bitmask(struct gl_shader *shader) {
 		char name[32];
 		glGetActiveUniformName(shader->prog, (GLuint)i, sizeof(name), NULL, name);
 		GLint loc = glGetUniformLocation(shader->prog, name);
-		assert(loc >= 0 && loc <= UNIFORM_TEXSIZE_LOC);
+		assert(loc >= 0 && loc < NUMBER_OF_UNIFORMS);
 		shader->uniform_bitmask |= 1U << loc;
 	}
 }
@@ -1110,6 +1114,10 @@ image_handle gl_new_image(backend_t *backend_data attr_unused,
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
 		glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR,
 		                 (GLfloat[]){0, 0, 0, 0});
+		// Use the red channel for all color channels, so the mask can kind of be
+		// used as a pixmap image.
+		glTexParameteriv(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_RGBA,
+		                 (GLint[]){GL_RED, GL_RED, GL_RED, GL_RED});
 	}
 	glBindTexture(GL_TEXTURE_2D, 0);
 	return (image_handle)tex;

commit 11c448e2a5134290ae1811f5b9ccfaff4647acc7

renderer: fix saved_image_blend
Oops, an oversight from previous commit, tint calculation for saved_image_blend
was wrong.

Fixes: 4275403f7244436591faacd6b90a3ee57de2465f

Signed-off-by: Yuxuan Shui <yshuiv7@gmail.com>

src/renderer/command_builder.c
@@ -67,12 +67,11 @@ commands_for_window_body(struct layer *layer, struct backend_command *cmd_base,
 	if (opacity < 1) {
 		opacity_saved = layer->opacity * layer->saved_image_blend / (1 - opacity);
 	}
+	struct color tint = {.red = 1. - dim, .green = 1. - dim, .blue = 1. - dim, .alpha = 1.};
 	struct backend_blit_args args_base = {
 	    .border_width = border_width,
 	    .corner_radius = layer->options.corner_radius,
-	    .tint = color_mult_alpha(
-	        (struct color){.red = 1. - dim, .green = 1. - dim, .blue = 1. - dim, .alpha = 1.},
-	        opacity),
+	    .tint = color_mult_alpha(tint, opacity),
 	    .scale = layer->scale,
 	    .effective_size = layer->window.size,
 	    .shader = shader != NULL ? shader->backend_shader : NULL,
@@ -102,7 +101,7 @@ commands_for_window_body(struct layer *layer, struct backend_command *cmd_base,
 		    .width = (int)(layer->window.size.width / w->saved_win_image_scale.width),
 		    .height = (int)(layer->window.size.height / w->saved_win_image_scale.height),
 		};
-		cmd->blit.tint = color_mult_alpha(cmd->blit.tint, opacity_saved);
+		cmd->blit.tint = color_mult_alpha(tint, opacity_saved);
 		cmd->blit.target_mask = &cmd->target_mask;
 		cmd->blit.scale = vec2_scale(cmd->blit.scale, w->saved_win_image_scale);
 		cmd -= 1;
@@ -121,7 +120,7 @@ commands_for_window_body(struct layer *layer, struct backend_command *cmd_base,
 	cmd->source = BACKEND_COMMAND_SOURCE_WINDOW;
 	cmd->blit = args_base;
 	cmd->blit.target_mask = &cmd->target_mask;
-	cmd->blit.tint = color_mult_alpha(cmd->blit.tint, w->frame_opacity * opacity);
+	cmd->blit.tint = color_mult_alpha(tint, w->frame_opacity * opacity);
 	cmd -= 1;
 	if (layer->saved_image_blend > 0) {
 		pixman_region32_copy(&cmd->target_mask, &cmd[1].target_mask);
@@ -134,7 +133,7 @@ commands_for_window_body(struct layer *layer, struct backend_command *cmd_base,
 		    .width = (int)(layer->window.size.width / w->saved_win_image_scale.width),
 		    .height = (int)(layer->window.size.height / w->saved_win_image_scale.height),
 		};
-		cmd->blit.tint = color_mult_alpha(cmd->blit.tint, w->frame_opacity * opacity);
+		cmd->blit.tint = color_mult_alpha(tint, w->frame_opacity * opacity);
 		cmd->blit.target_mask = &cmd->target_mask;
 		cmd->blit.scale = vec2_scale(cmd->blit.scale, w->saved_win_image_scale);
 		cmd -= 1;