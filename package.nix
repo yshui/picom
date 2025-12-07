c74e760e8cf9c90933e42176fb8cb5b45d562d39
Yuxuan Shui <yshuiv7@gmail.com>
2025-12-07 22:49:20 +0000

    flake: apply formatting
    Formatted by `nixfmt-tree`.
    
    Signed-off-by: Yuxuan Shui <yshuiv7@gmail.com>

--- package.nix
+++ package.nix
@@ -1,42 +1,44 @@
-{ asciidoctor
-, dbus
-, docbook_xml_dtd_45
-, docbook_xsl
-, fetchFromGitHub
-, llvmPackages
-, lib
-, libconfig
-, libdrm
-, libev
-, libGL
-, libepoxy
-, libX11
-, libxcb
-, libxdg_basedir
-, libXext
-, libxml2
-, libxslt
-, makeWrapper
-, meson
-, ninja
-, pcre2
-, pixman
-, pkg-config
-, python3
-, stdenv
-, uthash
-, xcbutil
-, xcbutilimage
-, xcbutilrenderutil
-, xorgproto
-, xwininfo
-, withDebug ? false
-, git-ignore-nix
-, devShell ? false
+{
+  asciidoctor,
+  dbus,
+  docbook_xml_dtd_45,
+  docbook_xsl,
+  fetchFromGitHub,
+  llvmPackages,
+  lib,
+  libconfig,
+  libdrm,
+  libev,
+  libGL,
+  libepoxy,
+  libX11,
+  libxcb,
+  libxdg_basedir,
+  libXext,
+  libxml2,
+  libxslt,
+  makeWrapper,
+  meson,
+  ninja,
+  pcre2,
+  pixman,
+  pkg-config,
+  python3,
+  stdenv,
+  uthash,
+  xcbutil,
+  xcbutilimage,
+  xcbutilrenderutil,
+  xorgproto,
+  xwininfo,
+  withDebug ? false,
+  git-ignore-nix,
+  devShell ? false,
 }:
 
 let
-  versionFromMeson = s: builtins.head (builtins.match "project\\('picom',.*version: *'([0-9.]*)'.*" s);
+  versionFromMeson =
+    s: builtins.head (builtins.match "project\\('picom',.*version: *'([0-9.]*)'.*" s);
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "picom";
@@ -46,7 +48,6 @@ stdenv.mkDerivation (finalAttrs: {
 
   strictDeps = true;
 
-
   nativeBuildInputs = [
     asciidoctor
     docbook_xml_dtd_45
@@ -55,13 +56,19 @@ stdenv.mkDerivation (finalAttrs: {
     meson
     ninja
     pkg-config
-  ] ++ (lib.optional devShell [
+  ]
+  ++ (lib.optional devShell [
     llvmPackages.clang-tools
     llvmPackages.clang-unwrapped.python
     llvmPackages.libllvm
-    (python3.withPackages (ps: with ps; [
-      xcffib pip dbus-next pygit2
-    ]))
+    (python3.withPackages (
+      ps: with ps; [
+        xcffib
+        pip
+        dbus-next
+        pygit2
+      ]
+    ))
   ]);
 
   buildInputs = [
@@ -102,7 +109,8 @@ stdenv.mkDerivation (finalAttrs: {
   postInstall = ''
     wrapProgram $out/bin/picom-trans \
       --prefix PATH : ${lib.makeBinPath [ xwininfo ]}
-  '' + lib.optionalString withDebug ''
+  ''
+  + lib.optionalString withDebug ''
     cp -r ../src $out/
   '';
 })
