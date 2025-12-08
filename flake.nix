38e2d1549e841891c4943ab61834a780d213919c
Yuxuan Shui <yshuiv7@gmail.com>
2025-12-08 00:17:00 +0000

    flake: bump nixpkgs to release-25.11
    Signed-off-by: Yuxuan Shui <yshuiv7@gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:nixos/nixpkgs/release-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/release-25.11";
     git-ignore-nix = {
       url = "github:hercules-ci/gitignore.nix/master";
       inputs.nixpkgs.follows = "nixpkgs";
